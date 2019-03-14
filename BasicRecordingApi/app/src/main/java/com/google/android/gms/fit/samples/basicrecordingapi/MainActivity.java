256a30b72a1f49b4c0ede52c1f409d9fa444ddd4
iMac3 <andreas.frenk@upswell.jp>
2019-03-14 18:44:57 +0900

    Weight (cannot read)
    Tried to implement weight, but cannot get information on the weight

--- BasicRecordingApi/app/src/main/java/com/google/android/gms/fit/samples/basicrecordingapi/MainActivity.java
+++ BasicRecordingApi/app/src/main/java/com/google/android/gms/fit/samples/basicrecordingapi/MainActivity.java
@@ -20,23 +20,47 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fit.samples.common.logger.Log;
 import com.google.android.gms.fit.samples.common.logger.LogView;
 import com.google.android.gms.fit.samples.common.logger.LogWrapper;
 import com.google.android.gms.fit.samples.common.logger.MessageOnlyLogFilter;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.data.Subscription;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.fitness.result.DataReadResult;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static java.text.DateFormat.getDateInstance;
 
 
 /**
@@ -56,6 +80,21 @@ public class MainActivity extends AppCompatActivity {
         // This method sets up our custom logger, which will print all log messages to the device
         // screen, as well as to adb logcat.
         initializeLogging();
+        final Button read_data = findViewById(R.id.read_data);
+        final Button insert_data = findViewById(R.id.insert_data);
+
+        read_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readData();
+            }
+        });
+        insert_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertData();
+            }
+        });
 
         FitnessOptions fitnessOptions =
                 FitnessOptions.builder().addDataType(DataType.TYPE_ACTIVITY_SAMPLES).build();
@@ -68,15 +107,20 @@ public class MainActivity extends AppCompatActivity {
                     REQUEST_OAUTH_REQUEST_CODE,
                     GoogleSignIn.getLastSignedInAccount(this),
                     fitnessOptions);
+            Log.i(TAG,"requesting permission");
         } else {
             subscribe();
+            Log.i(TAG,"subscribe");
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG,"onactivityresult");
         if (resultCode == Activity.RESULT_OK) {
+            Log.i(TAG,"result code ok");
             if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
+                Log.i(TAG,"request code ok");
                 subscribe();
             }
         }
@@ -90,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
      * a special success code.
      */
     public void subscribe() {
+        Log.i(TAG,"subscribe function started");
         // To create a subscription, invoke the Recording API. As soon as the subscription is
         // active, fitness data will start recording.
         // [START subscribe_to_datatype]
@@ -116,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void dumpSubscriptionsList() {
         // [START list_current_subscriptions]
+        Log.i(TAG,"dumpsubscriptionlist");
         Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .listSubscriptions(DataType.TYPE_ACTIVITY_SAMPLES)
                 .addOnSuccessListener(new OnSuccessListener<List<Subscription>>() {
@@ -129,7 +175,63 @@ public class MainActivity extends AppCompatActivity {
                 });
         // [END list_current_subscriptions]
     }
+//
+    private void readData(){
+        // Setting a start and end date using a range of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
 
+        java.text.DateFormat dateFormat = getDateInstance();
+        Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+        Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest dataReadRequest = new DataReadRequest.Builder()
+                .read(DataType.TYPE_WEIGHT)
+                .setTimeRange(1, cal.getTimeInMillis(), TimeUnit.MILLISECONDS)
+                .setLimit(1)
+                .build();
+
+        Log.i(TAG,"weight data: "+dataReadRequest);
+
+    }
+
+    
+    private void insertData(){
+        //Insert Data to google
+        // Set a start and end time for our data, using a start time of 1 hour before this moment.
+        Log.i(TAG, "Insert Data started ");
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.HOUR_OF_DAY, -1);
+        long startTime = cal.getTimeInMillis();
+
+// Create a data source
+        DataSource dataSource =
+                new DataSource.Builder()
+                        .setAppPackageName(this)
+                        .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                        .setStreamName(TAG + " - step count")
+                        .setType(DataSource.TYPE_RAW)
+                        .build();
+
+// Create a data set
+        int stepCountDelta = 950;
+        DataSet dataSet = DataSet.create(dataSource);
+// For each data point, specify a start time, end time, and the data value -- in this case,
+// the number of new steps.
+        DataPoint dataPoint =
+                dataSet.createDataPoint().setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS);
+        dataPoint.getValue(Field.FIELD_STEPS).setInt(stepCountDelta);
+        dataSet.add(dataPoint);
+        Log.i(TAG, "dataset added");
+
+    }
     /**
      * Cancels the ACTIVITY_SAMPLE subscription by calling unsubscribe on that {@link DataType}.
      */
@@ -160,6 +262,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG,"oncreateoptionsmenu");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
